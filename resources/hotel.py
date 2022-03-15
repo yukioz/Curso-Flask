92069544cfd868edfdab93d77587e9c6e7a16d79
yukioz <yukio.link@gmail.com>
2022-03-15 20:14:04 -0300

    filter hoteis by query params.
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,12 +1,79 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+
+def normalize_path_params(cidade=None, estrelas_min=0, estrelas_max=5, diaria_min = 0, diaria_max = 10000, limit = 50, offset=0, **dados):
+    
+    # limit = maximo de resultado por consulta
+    # offset = quantos devo pular? limit * número da página
+    if cidade:
+        return {
+            "estrelas_min": estrelas_min,
+            "estrelas_max": estrelas_max,
+            "diaria_min" : diaria_min,
+            "diaria_max" : diaria_max,
+            "cidade": cidade,
+            "limit" : limit,
+            "offset": offset,
+        }
+    return {
+            "estrelas_min": estrelas_min,
+            "estrelas_max": estrelas_max,
+            "diaria_min" : diaria_min,
+            "diaria_max" : diaria_max,
+            "limit" : limit,
+            "offset": offset,
+        }
+
+# path /hoteis?cidade=Rio de Janeiro&estrelas+min=4&diaria_max400
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estrelas_min', type=float)
+path_params.add_argument('estrelas_max', type=float)
+path_params.add_argument('diaria_min', type=float)
+path_params.add_argument('diaria_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
 
 class Hoteis(Resource):
 
     # Compressão de lista python
     def get(self):
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+
+        dados = path_params.parse_args()
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if not parametros.get('cidade'):
+            consulta = "SELECT * FROM hoteis \
+                WHERE (estrelas >= ? and estrelas < ?) \
+                and (diaria >= ? and diaria < ?) \
+                LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])   
+            resultado = cursor.execute(consulta, tupla)
+        else:
+            consulta = "SELECT * FROM hoteis \
+                WHERE (estrelas >= ? and estrelas < ?) \
+                and (diaria >= ? and diaria < ?) \
+                and cidade = ? \
+                LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])   
+            resultado = cursor.execute(consulta, tupla)
+
+        hoteis = []
+        for linha in resultado:
+            hoteis.append({
+                'hotel_id': linha[0],
+                'nome': linha[1],
+                'estrelas': linha[2],
+                'diaria': linha[3],
+                'cidade': linha[4]
+            })
+
+        return {'hoteis': hoteis}
 
 class Hotel(Resource):
     atributos = reqparse.RequestParser()
